7925eda34880b797181d53a6eb452805d9d223f3
Mon Name <yassinechliah75014@gmail.com>
2025-03-13 00:17:21 +0100

    update database.js

--- gestion-produits/src/config/database.js
+++ gestion-produits/src/config/database.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "postgres",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+    port: process.env.POSTGRES_PORT || 5432,
+    logging: false,
+  }
+);
 
 sequelize
   .authenticate()
