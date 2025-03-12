65601b5e8441e4890c60cdbde7ba81cd4df980fe
Mon Name <yassinechliah75014@gmail.com>
2025-03-12 23:47:44 +0100

    productRoutes.js update

--- gestion-produits/src/routes/productRoutes.js
+++ gestion-produits/src/routes/productRoutes.js
@@ -6,5 +6,6 @@ router.post("/", productController.createProduct);
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
 router.put("/:id", productController.updateProduct);
+router.delete("/:id", productController.deleteProduct);
 
 module.exports = router;
