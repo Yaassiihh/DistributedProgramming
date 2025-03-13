09e7576803e28fdaba728b8b4f51b98fa0b5d6bb
Mon Name <yassinechliah75014@gmail.com>
2025-03-13 01:39:43 +0100

    update database.js, POSTGRES_HOST --> DB_HOST

--- gestion-produits/src/config/database.js
+++ gestion-produits/src/config/database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.POSTGRES_USER,
   process.env.POSTGRES_PASSWORD,
   {
-    host: process.env.POSTGRES_HOST,
+    host: process.env.DB_HOST,
     dialect: "postgres",
     port: process.env.POSTGRES_PORT || 5432,
     logging: false,
