e4150ad133af5de4919700c190299164aea6d016
Mon Name <yassinechliah75014@gmail.com>
2025-05-13 17:53:06 +0200

    add gestion-commandes folder, file structure

---
+++ gestion-commandes/src/config/database.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
+    logging: false,
+  }
+);
+
+sequelize.authenticate()
+  .then(() => console.log('✅ Connexion à PostgreSQL réussie !'))
+  .catch((err) => console.error('❌ Erreur de connexion à la base de données :', err));
+
+module.exports = sequelize;
