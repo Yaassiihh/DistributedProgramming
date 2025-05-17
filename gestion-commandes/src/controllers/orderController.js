6e44f5aff2defe7fe35013debff69d76cb3764a0
Mon Name <yassinechliah75014@gmail.com>
2025-05-17 18:05:02 +0200

    last commit before the front end

--- gestion-commandes/src/controllers/orderController.js
+++ gestion-commandes/src/controllers/orderController.js
@@ -1,13 +1,34 @@
 const Order = require('../models/order');
+const axios = require("axios");
 
 exports.createOrder = async (req, res) => {
   try {
     const { productId, quantity, status } = req.body;
+
+    const productResponse = await axios.get(`http://api-service:3000/products/${productId}`);
+    console.log("Produit reçu :", productResponse.data); 
+    const product = productResponse.data;
+
+    if (!product) {
+      return res.status(404).json({ error: "Produit introuvable." });
+    }
+
+    if (product.stock < quantity) {
+      return res.status(400).json({ error: "Stock insuffisant." });
+    }
+
     const order = await Order.create({ productId, quantity, status });
+
+    await axios.put(`http://api-service:3000/products/${productId}`, {
+      ...product,
+      stock: product.stock - quantity
+    });
+
     res.status(201).json(order);
   } catch (error) {
-    res.status(500).json({ error: 'Erreur lors de la création de la commande.' });
-  }
+  console.error("❌ Erreur lors de la création de la commande :", error);
+  res.status(500).json({ error: error.message || "Erreur lors de la création de la commande." });
+}
 };
 
 exports.getAllOrders = async (req, res) => {
